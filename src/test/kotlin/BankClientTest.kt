99a38aa1cc456360d82f916460902be02df711ee
David MA <ha_257@hotmail.com>
2024-10-25 00:32:57 +0200

    [GREEN] Bank client should deposit money in bank account which current balance is not empty

--- src/test/kotlin/BankClientTest.kt
+++ src/test/kotlin/BankClientTest.kt
@@ -14,4 +14,28 @@ class BankClientTest {
         // THEN
         assertEquals(BankClient(AccountBalance(1)), bankClientWithOneInBalance)
     }
+
+    @Test
+    fun shouldDepositMoneyInBankAccountWithOneInBalance() {
+        // GIVEN
+        val bankClientWithOneInBalance = BankClient(AccountBalance(1))
+
+        // WHEN
+        val bankClientWithTwoInBalance = bankClientWithOneInBalance.deposit(1)
+
+        // THEN
+        assertEquals(BankClient(AccountBalance(2)), bankClientWithTwoInBalance)
+    }
+
+    @Test
+    fun shouldDepositMoneyInBankAccountWithTwoInBalance() {
+        // GIVEN
+        val bankClientWithTwoInBalance = BankClient(AccountBalance(2))
+
+        // WHEN
+        val bankClientWithThreeInBalance = bankClientWithTwoInBalance.deposit(1)
+
+        // THEN
+        assertEquals(BankClient(AccountBalance(3)), bankClientWithThreeInBalance)
+    }
 }
